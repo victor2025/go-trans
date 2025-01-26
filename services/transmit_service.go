c6417923f91cefcc06fe7838bc438a1eb2f2dbf8
victor2022 <lihengwei111@outlook.com>
2025-01-26 13:13:57 +0800

    修正文件传输服务器启停逻辑

--- services/transmit_service.go
+++ services/transmit_service.go
@@ -5,10 +5,12 @@ import (
 	"log"
 )
 
-/**
-  @author: victor2022
-  @since: 2025/1/2
-  传输管理服务，用来管理接收服务器与发送服务器
+/*
+*
+
+	@author: victor2022
+	@since: 2025/1/2
+	传输管理服务，用来管理接收服务器与发送服务器
 */
 type TransmitService struct {
 	receiveServerIsOn bool
@@ -29,18 +31,19 @@ func (s *TransmitService) StartReceiveServer(port, basePath string) {
 		s.receiveServerIsOn = true
 		s.receiveHandler = receiveHandler
 	} else {
-		log.Printf("Transmit server is already on, port:%s", port)
+		log.Printf("transmit server is already on, port:%s", port)
 	}
 }
 
 // StopReceiveServer stop server
 func (s *TransmitService) StopReceiveServer() {
-	log.Println("Stopping server")
-	if !s.receiveServerIsOn {
+	log.Println("stopping transmit server")
+	if s.receiveServerIsOn {
 		s.receiveServerIsOn = false
 	}
 	if s.receiveHandler != nil {
 		s.receiveHandler.StopHandle()
 	}
 	s.receiveHandler = nil
+	log.Println("transmit server stopped")
 }
