9c1424972920643e8202014de83cc2ee58c1e398
victor2022 <lihengwei111@outlook.com>
2025-01-28 18:38:15 +0800

    补充注释

--- http/api/system/control.go
+++ http/api/system/control.go
@@ -6,17 +6,18 @@ import (
 	"go-trans/services"
 )
 
-/*
-*
-
-	@author: victor2022
-	@since: 2025/1/5
+/**
+@author: victor2022
+@since: 2025/1/5
 */
+
+// 启动文件接收服务器
 func startTransmitServer(c *gin.Context) {
 	services.GetServiceContext().StartReceiveServer()
 	response.NewSuccessResponse(c, "start success")
 }
 
+// 关闭文件接收服务器
 func stopTransmitServer(c *gin.Context) {
 	services.GetServiceContext().StopReceiveServer()
 	response.NewSuccessResponse(c, "stop success")
